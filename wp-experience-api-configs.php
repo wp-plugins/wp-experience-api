682ec1cd92e9c2e9183ab904c91ccf4916f142ba
loongchan <loongchan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-11 21:04:24 +0000

    initial commit of plugin.  Still need to add tag.
    git-svn-id: https://plugins.svn.wordpress.org/wp-experience-api/trunk@1158250 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-experience-api-configs.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * These are just default constants that can be used anywhere
+ *
+ */
+define( 'WP_XAPI_DEFAULT_ACTOR_ACCOUNT_HOMEPAGE', 'http://www.example.org' );
+define( 'WP_XAPI_DEFAULT_XAPI_VERSION', '1.0.0' );
+define( 'WP_XAPI_DEFAULT_ACTOR_ACCOUNT_NAME', 'unique_id' );
+define( 'WP_XAPI_MINIMUM_PHP_VERSION', '5.4' );
